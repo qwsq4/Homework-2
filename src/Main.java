c4a5db48714c65fe6be13073ccdd6d8d51c9018b
qwsq4 <qwsq1437@gmail.com>
2022-09-11 20:29:43 +0800

    Сделано ДЗ 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        double dog = 8;
+        var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
         System.out.println(dog);
