b878dc08d01eb5c95fd34ec12f83e6eb1b191bdc
qwsq4 <qwsq1437@gmail.com>
2022-09-11 20:28:34 +0800

    Сделано ДЗ 3

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,21 @@
 public class Main {
-    public static void main(String[] args) {
-        var friend = 19;
-        friend = friend + 2;
-        friend = friend / 7;
-        System.out.println(friend);
-        
-        var frog = 3.5;
-        System.out.println(frog);
-        frog = frog * 10;
-        System.out.println(frog);
-        frog = frog / 3.5;
-        System.out.println(frog);
-        frog = frog + 4;
-        System.out.println(frog);
+    public static void main(String[] args){
+    var boxerWeight1 = 78.2;
+    var boxerWeight2 = 82.7;
+    var boxerTotalWeight = boxerWeight1 + boxerWeight2;
+        System.out.println("Общий вес бойцов " + boxerTotalWeight + " кг");
+
+    var boxerWeightDifference = boxerWeight2 - boxerWeight1;
+        System.out.println("Разница в весе " + boxerWeightDifference + " кг");
+    var boxerWeightDifference1 = boxerWeight2 % boxerWeight1;
+        System.out.println("Разница в весе " + boxerWeightDifference1 + " кг");
+
+    var totalWorkTime = 640;
+    var workerWorkTime = 8;
+    var totalWorkers = totalWorkTime / workerWorkTime;
+        System.out.println("Всего работников в компании - " + totalWorkers + " человек");
+    var totalWorkers1 = totalWorkers + 94;
+    var totalWorkTime1 = totalWorkers1 * workerWorkTime;
+        System.out.println("Если в компании работает " + totalWorkers1 + " человек, то всего " +totalWorkTime1 + " часов работы может быть поделено между сотрудниками");
     }
 }
