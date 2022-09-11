a537823f886cdf2e8c39a698f19845fecb106cbe
qwsq4 <113195645+qwsq4@users.noreply.github.com>
2022-09-11 17:01:36 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        
-       var friend = 19;
+        var friend = 19;
         friend = friend + 2;
         friend = friend / 7;
         System.out.println(friend);
