18869493de56f992411eda368e55367c0e5f790f
qwsq4 <qwsq1437@gmail.com>
2022-09-11 20:57:59 +0800

    Сделано ДЗ 1

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         //Задача 4
-        System.out.println("Задача 4");
+        System.out.println("   Задача 4");
         var friend = 19;
         friend = friend + 2;
         friend = friend / 7;
         System.out.println(friend);
 
         //Задача 5
-        System.out.println("Задача 5");
+        System.out.println("   Задача 5");
         var frog = 3.5;
         System.out.println(frog);
         frog = frog * 10;
